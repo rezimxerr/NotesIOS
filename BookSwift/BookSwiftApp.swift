e5090b9b7af9e27173590f6a5ecafff75bb7fcc2
Khakim <luthfiel99@gmail.com>
2023-11-02 19:34:37 +0700

    Initial Commit

---
+++ BookSwift/BookSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSwiftApp.swift
+//  BookSwift
+//
+//  Created by Khakim on 02/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BookSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
